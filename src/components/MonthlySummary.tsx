4f8736816a7d3d370c488d6c5bd14bc1df430b15
petrius <maria.petrius@gmail.com>
2025-09-07 11:29:13 +0200

    add component

--- src/components/MonthlySummary.tsx
+++ src/components/MonthlySummary.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react"
+import { getPaymentSummary, MontlySummary } from "../services/paymentMonthlySummary";
+
 export const MonthlySummary = () => {
+
+    const [monthlySummaries, setMonthlySummaries] = useState<MontlySummary[]>([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        getPaymentSummary()
+        .then(data => setMonthlySummaries(data))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
+    }, []);
     return (
-        <div>
-            <h1>Hello From Monthly Summary</h1>
+        <div>        
+            <h1>Payment Journal</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Month</th>
+                        <th>Payments</th>
+                        <th>Number Of Payment</th>
+                        <th>Paied in total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        monthlySummaries.map(summary => (
+                            <tr key={summary.yearMonth}>
+                                <td>{summary.yearMonth}</td>
+                                <td>{summary.allPaymentsInMonth.map(payment => (
+                                    <p>{payment.amount}</p>
+                                ))}
+                                </td>
+                                <td>{summary.paymentCount}</td>
+                                <td>{summary.totalAmount}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
