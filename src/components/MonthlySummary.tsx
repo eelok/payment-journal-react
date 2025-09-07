636671acb57cee3c1b9273105b6ed76643ba4a20
petrius <maria.petrius@gmail.com>
2025-09-07 14:20:49 +0200

    wip

--- src/components/MonthlySummary.tsx
+++ src/components/MonthlySummary.tsx
@@ -1,16 +1,26 @@
 import { useEffect, useState } from "react"
 import { getPaymentSummary, MontlySummary } from "../services/paymentMonthlySummary";
+import { error } from "console";
 
 export const MonthlySummary = () => {
 
     const [monthlySummaries, setMonthlySummaries] = useState<MontlySummary[]>([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string>();
+
     useEffect(() => {
         getPaymentSummary()
         .then(data => setMonthlySummaries(data))
-        .catch(error => console.log(error))
+        .catch(error => setError("Failed to load data. Check the backend"))
         .finally(() => setLoading(false));
     }, []);
+
+    if(loading) {
+        return <div>Loading the data ....</div>
+    }
+    if(error) {
+        return<div style={{color: 'red'}}>Error: {error}</div>
+    }
     return (
         <div>        
             <h1>Payment Journal</h1>
@@ -29,7 +39,7 @@ export const MonthlySummary = () => {
                             <tr key={summary.yearMonth}>
                                 <td>{summary.yearMonth}</td>
                                 <td>{summary.allPaymentsInMonth.map(payment => (
-                                    <p>{payment.amount}</p>
+                                    <p key={payment.id}>{payment.amount}</p>
                                 ))}
                                 </td>
                                 <td>{summary.paymentCount}</td>
